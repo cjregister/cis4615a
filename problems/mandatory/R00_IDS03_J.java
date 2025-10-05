2ec96445637c4e2313ed6918270517e2358794bf
Cayleb Register <cayleb.register@gmail.com>
2025-10-05 03:57:33 -0400

    IDS03-J compliant: sanitize input; avoid logging secrets; parameterized logging

--- problems/mandatory/R00_IDS03_J.java
+++ problems/mandatory/R00_IDS03_J.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class R00_IDS03_J {
   private static final Logger LOG = Logger.getLogger(R00_IDS03_J.class.getName());
+
+  // Remove control chars ( to prevent log forging
+  private static String sanitizeForLog(String s) {
+    if (s == null) return null;
+    s = s.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ');
+    return s.replaceAll("[\\x00-\\x1F\\x7F]", "?");
+  }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -11,9 +19,10 @@ public class R00_IDS03_J {
     System.out.print("Enter password: ");
     String pwd = sc.nextLine();
 
-    // NONCOMPLIANT: logs unsanitized user input and a secret
-    LOG.info("Login attempt by user=" + user + " password=" + pwd);
+    // COMPLIANT: do not log the password, sanitize user-controlled data
+    LOG.log(Level.INFO, "Login attempt user={0} pwd_length={1}",
+        new Object[]{ sanitizeForLog(user), (pwd == null ? 0 : pwd.length()) });
 
-    System.out.println("Logged (noncompliant).");
+    System.out.println("Logged (compliant).");
   }
 }
