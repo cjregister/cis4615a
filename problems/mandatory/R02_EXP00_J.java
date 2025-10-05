0c9aa0cdfc743c34be44536f62153e53665e2080
Cayleb Register <cayleb.register@gmail.com>
2025-10-05 04:08:00 -0400

    EXP00-J compliant, check boolean, fallback with deleteOnExit

--- problems/mandatory/R02_EXP00_J.java
+++ problems/mandatory/R02_EXP00_J.java
@@ -4,8 +4,13 @@ public class R02_EXP00_J {
   public static void main(String[] args) throws Exception {
     File f = File.createTempFile("exp00", ".tmp");
     System.out.println("Temp: " + f.getAbsolutePath());
-    // NONCOMPLIANT: ignore return value, risky
-    f.delete();
-    System.out.println("Done, maybe deleted");
+    // COMPLIANT: check and act on return value
+    boolean deleted = f.delete();
+    if (!deleted) {
+      System.err.println("Delete failed, marking deleteOnExit");
+      f.deleteOnExit();
+    } else {
+      System.out.println("Deleted ok");
+    }
   }
 }
